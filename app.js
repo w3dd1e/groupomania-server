593ce2d2a4a8abb6468c6866351d35917687855e
kelly <kelly.weddle@gmail.com>
2024-02-04 11:33:04 -0600

    Add "/auth" route to app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const logger = require('morgan');
 const cors = require('cors');
-const pool = require('./database/db');
 
 const app = express();
 
@@ -17,15 +16,17 @@ app.use(cookieParser()); //Parse cookies in headers; req.cookies
 app.use(express.static(path.join(__dirname, 'public'))); //Serve static files from 'public' directory
 app.use(cors()) //All CORS Requests
 
+app.use("/auth", usersRouter);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-/*app.use("/auth", userRoutes);
+/*
 app.get("/posts", postRoutes);
 app.get("/posts/:id", postRoutes);
 app.post("/posts", postRoutes);
 app.delete("/posts/:id", postsRoutes);
 app.put("/posts/:id", sauceRoutes);
 app.post("/posts/:id/like", likeRoutes); */
+
 
 module.exports = app;
