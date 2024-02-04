dd059dadd3065c43c147b5fe2fcf76a387b57f52
kelly <kelly.weddle@gmail.com>
2024-02-03 18:14:12 -0600

    Add CORS headers to allow requests from any origin -Replace var with const

--- app.js
+++ app.js
@@ -12,7 +12,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Allow requests from any origin, set headers and methods allowed for request
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use("/api/auth", userRoutes);
+app.get("/api/sauces", sauceRoutes);
+app.get("/api/sauces/:id", sauceRoutes);
+app.post("/api/sauces", sauceRoutes);
+app.delete("/api/sauces/:id", sauceRoutes);
+app.put("/api/sauces/:id", sauceRoutes);
+app.post("/api/sauces/:id/like", likeRoutes);
 
 module.exports = app;
