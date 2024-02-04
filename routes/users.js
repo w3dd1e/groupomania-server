d272f0253a4c633486223bed3f61f41795b2cc17
kelly <kelly.weddle@gmail.com>
2024-02-04 11:33:12 -0600

    Add signup and login routes to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+ 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
