994b268b1c6dfbe7107b04ce745d5dd2253958cf
kelly <kelly.weddle@gmail.com>
2024-02-10 11:52:23 -0600

    Add Sequelize to db

--- database/db.js
+++ database/db.js
@@ -1,11 +1,24 @@
-const Pool = require('pg').Pool;
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv").config();
 
-const pool = new Pool({
-	user: process.env.PGUSER,
-	password: process.env.PGPASSWORD,
-	host: process.env.PGHOST,
-	port: process.env.PGPORT,
-	database: process.env.PGDATABASE,
-});
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+  }
+);
 
-module.exports = pool;
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(" Database connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+module.exports = sequelize;
