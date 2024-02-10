a81d585bbefad36ed1d323d0aef919ae44c185bf
w3dd1e <kelly.weddle@gmail.com>
2024-02-09 21:59:47 -0600

    Update app.js with required dependencies and fix formatting

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const logger = require('morgan');
 const cors = require('cors');
-
+const db = require('./database/db');
 const app = express();
-
+const dotenv = require('dotenv').config();
 
 app.use(logger('dev')); //Log response status to console
 app.use(express.json()); //Parse incoming JSON requests; req.body
 app.use(express.urlencoded({ extended: false })); //Parse requests app/x-www-form-urlencoded; req.body
 app.use(cookieParser()); //Parse cookies in headers; req.cookies
 app.use(express.static(path.join(__dirname, 'public'))); //Serve static files from 'public' directory
-app.use(cors()) //All CORS Requests
+app.use(cors()); //All CORS Requests
 
-app.use("/auth", usersRouter);
+app.use('/auth', usersRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -27,6 +27,5 @@ app.post("/posts", postRoutes);
 app.delete("/posts/:id", postsRoutes);
 app.put("/posts/:id", sauceRoutes);
 app.post("/posts/:id/like", likeRoutes); */
-
 
 module.exports = app;
