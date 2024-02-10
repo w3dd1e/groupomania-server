061ade7bc79ece5f57796b55748eecfc4d237207
kelly <kelly.weddle@gmail.com>
2024-02-10 17:49:30 -0600

    Create post routing

---
+++ routes/posts.rt.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const postCtrl = require("../controllers/posts.ctrl");
+
+router.get("/posts", auth, postCtrl.getAllPosts);
+router.post("/posts", auth, postCtrl.createPost);
+router.get("/posts/:id", auth, postCtrl.getOnePost);
+router.put("/posts/:id", auth, postCtrl.updatePost);
+router.delete("/posts/:id", auth, postCtrl.deletePost);
+
+module.exports = router;
