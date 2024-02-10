dbcae4dcf53a146057b354d37bc5500d8f22004f
kelly <kelly.weddle@gmail.com>
2024-02-10 11:51:41 -0600

    Refactor code for better organization

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const logger = require('morgan');
-const cors = require('cors');
-const db = require('./database/db');
-const app = express();
-const dotenv = require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-app.use(logger('dev')); //Log response status to console
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users.rt");
+
+const app = express();
+
+app.use(logger("dev")); //Log response status to console
 app.use(express.json()); //Parse incoming JSON requests; req.body
 app.use(express.urlencoded({ extended: false })); //Parse requests app/x-www-form-urlencoded; req.body
 app.use(cookieParser()); //Parse cookies in headers; req.cookies
-app.use(express.static(path.join(__dirname, 'public'))); //Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, "public"))); //Serve static files from 'public' directory
 app.use(cors()); //All CORS Requests
 
-app.use('/auth', usersRouter);
+app.use("/auth", usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 /*
 app.get("/posts", postRoutes);
 app.get("/posts/:id", postRoutes);
