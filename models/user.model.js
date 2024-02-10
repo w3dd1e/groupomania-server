8954070885b22491fcae485c29629b71c87e8307
kelly <kelly.weddle@gmail.com>
2024-02-10 16:29:54 -0600

    Update primary key to user_id

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,11 @@ const sequelize = require("../database/db");
 const { Sequelize, DataTypes } = require("sequelize");
 
 const User = sequelize.define("user", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
