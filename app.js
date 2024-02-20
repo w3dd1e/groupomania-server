3799aa2df7f7fdaf7dbd7de118e4c6abf4c4629f
kelly <kelly.weddle@gmail.com>
2024-02-19 21:41:44 -0600

    Add CORS headers to allow requests from any origin

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ app.use(cookieParser()); //Parse cookies in headers; req.cookies
 app.use(express.static(path.join(__dirname, "public"))); //Serve static files from 'public' directory
 app.use(cors()); //All CORS Requests
 
+//Allow requests from any origin, set headers and methods allowed for request
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/auth", usersRouter);
 
 app.use("/", indexRouter);
