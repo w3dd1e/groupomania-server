23f6c440387fa2cca8949e8c4e050b5626302f84
w3dd1e <kelly.weddle@gmail.com>
2024-03-14 00:49:20 -0500

    Add post_id to respnose

--- controllers/posts.ctrl.js
+++ controllers/posts.ctrl.js
@@ -1,88 +1,88 @@
-const Post = require("../models/post.model");
-const jwt = require("jsonwebtoken");
+const Post = require('../models/post.model');
+const jwt = require('jsonwebtoken');
 
 exports.createPost = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
-  const userId = decodedToken.userId;
-  const post = await Post.create({
-    user_id: userId,
-    headline: req.body.headline,
-    content: req.body.content,
-  });
-  post
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "Post saved successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-      console.log(error);
-    });
+	const token = req.headers.authorization.split(' ')[1];
+	const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+	const userId = decodedToken.userId;
+	const post = await Post.create({
+		user_id: userId,
+		headline: req.body.headline,
+		content: req.body.content,
+	});
+	post.save()
+		.then(() => {
+			res.status(201).json({
+				postId: post.post_id,
+				message: 'Post saved successfully!',
+			});
+		})
+		.catch((error) => {
+			res.status(400).json({
+				error: error,
+			});
+			console.log(error);
+		});
 };
 
 exports.getOnePost = (req, res, next) => {
-  Post.findOne({
-    where: {
-      post_id: req.params.id,
-    },
-  })
-    .then((post) => {
-      res.status(200).json(post);
-    })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
-      });
-    });
+	Post.findOne({
+		where: {
+			post_id: req.params.id,
+		},
+	})
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((error) => {
+			res.status(404).json({
+				error: error,
+			});
+		});
 };
 
 exports.updatePost = async (req, res, next) => {
-  await Post.update(
-    { headline: req.body.headline, content: req.body.content },
-    { where: { post_id: req.params.id } }
-  )
-    .then(() => {
-      res.status(201).json({
-        message: "Post updated successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+	await Post.update(
+		{ headline: req.body.headline, content: req.body.content },
+		{ where: { post_id: req.params.id } }
+	)
+		.then(() => {
+			res.status(201).json({
+				message: 'Post updated successfully!',
+			});
+		})
+		.catch((error) => {
+			res.status(400).json({
+				error: error,
+			});
+		});
 };
 
 exports.deletePost = async (req, res, next) => {
-  await Post.destroy({ where: { post_id: req.params.id } })
+	await Post.destroy({ where: { post_id: req.params.id } })
 
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+		.then(() => {
+			res.status(200).json({
+				message: 'Deleted!',
+			});
+		})
+		.catch((error) => {
+			console.log(error);
 
-      res.status(400).json({
-        error: error,
-      });
-    });
+			res.status(400).json({
+				error: error,
+			});
+		});
 };
 
 exports.getAllPosts = (req, res, next) => {
-  Post.findAll()
-    .then((posts) => {
-      res.status(200).json(posts);
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+	Post.findAll()
+		.then((posts) => {
+			res.status(200).json(posts);
+		})
+		.catch((error) => {
+			res.status(400).json({
+				error: error,
+			});
+		});
 };
