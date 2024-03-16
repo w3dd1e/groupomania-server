98b16dc9642a96db33e68793351720e7a80e6467
w3dd1e <kelly.weddle@gmail.com>
2024-03-16 13:41:47 -0500

    Refactor user name in model

--- models/user.model.js
+++ models/user.model.js
@@ -1,62 +1,58 @@
-const sequelize = require("../database/db");
-const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require('../database/db');
+const { Sequelize, DataTypes } = require('sequelize');
 
-const User = sequelize.define("user", {
-  user_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
+const User = sequelize.define('user', {
+	user_id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	username: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
 
-  profileImage: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: "localhost:3000/public/images/newUserIcon.png",
-  },
+	profileImage: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		defaultValue: 'localhost:3000/public/images/newUserIcon.png',
+	},
 
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "",
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "",
-  },
-  department: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "",
-  },
-  location: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: "",
-  },
-  bio: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-    defaultValue: "",
-  },
+	fullName: {
+		type: DataTypes.STRING,
+		allowNull: true,
+		defaultValue: '',
+	},
+
+	department: {
+		type: DataTypes.STRING,
+		allowNull: true,
+		defaultValue: '',
+	},
+	location: {
+		type: DataTypes.STRING,
+		allowNull: true,
+		defaultValue: '',
+	},
+	bio: {
+		type: DataTypes.TEXT,
+		allowNull: true,
+		defaultValue: '',
+	},
 });
 
 (async () => {
-  await User.sync();
+	await User.sync();
 })();
 
 console.log(User === sequelize.models.User);
