875fec70a1d90bcf65c1899b4077243f33f3eb81
kelly <kelly.weddle@gmail.com>
2024-03-27 01:28:45 -0500

    Remove Drizzle

--- database/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-import * as schema from '../models/schema';
-
-const pool = new Pool({
-	connectionString: process.env.DBURL,
-});
-
-export const db = drizzle(pool, { schema });
