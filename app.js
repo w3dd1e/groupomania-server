3c119db5997a914d4016d659d169d2ba2d2d1496
w3dd1e <kelly.weddle@gmail.com>
2024-04-03 22:01:46 -0500

    Update app.js to parse larger requests -remove duplciate CORS functions

--- app.js
+++ app.js
@@ -12,30 +12,14 @@ const profileRouter = require('./routes/profiles.rt');
 const app = express();
 
 app.use(logger('dev')); //Log response status to console
-app.use(express.json()); //Parse incoming JSON requests; req.body
-app.use(express.urlencoded({ extended: false })); //Parse requests app/x-www-form-urlencoded; req.body
+app.use(express.json({ limit: '50mb' })); //Parse incoming JSON requests; req.body
+app.use(express.urlencoded({ limit: '50mb', extended: false })); //Parse requests app/x-www-form-urlencoded; req.body
 app.use(cookieParser()); //Parse cookies in headers; req.cookies
 app.use(express.static(path.join(__dirname, 'public'))); //Serve static files from 'public' directory
 app.use(cors()); //All CORS Requests
 
-//Allow requests from any origin, set headers and methods allowed for request
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-	);
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-	);
-	next();
-});
+app.use('public/images', express.static(path.join(__dirname, 'public/images')));
 
-/**
- * FEATURE TO BE INCLUDED IN FUTURE VERSION OF APP
- * app.use("public/images", express.static(path.join(__dirname, "images")));
- */
 app.use('/auth', usersRouter);
 
 app.use('/users', usersRouter);
