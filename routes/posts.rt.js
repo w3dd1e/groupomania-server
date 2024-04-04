4d16e7d0abd849d95de2c19afb4d104a0a87d6d4
w3dd1e <kelly.weddle@gmail.com>
2024-04-03 22:03:06 -0500

    Add Multer middleware to posts routes

--- routes/posts.rt.js
+++ routes/posts.rt.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('../middleware/multer-config');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
@@ -6,9 +7,9 @@ const postCtrl = require('../controllers/posts.ctrl');
 
 //Route request to post controllers
 router.get('/posts', auth, postCtrl.getAllPosts);
-router.post('/posts', auth, postCtrl.createPost);
+router.post('/posts', auth, multer, postCtrl.createPost);
 router.get('/posts/:id', auth, postCtrl.getOnePost);
-router.put('/posts/:id', auth, postCtrl.updatePost);
+router.put('/posts/:id', auth, multer, postCtrl.updatePost);
 router.delete('/posts/:id', auth, postCtrl.deletePost);
 
 module.exports = router;
